99625a06a00752b52f7938c3f719630cfe84f181
Ali ÇİÇEK <m.alicicek18@hotmail.com>
2022-04-26 08:55:57 +0300

    -

---
+++ FastModularExponentiation/Program.cs
@@ -0,0 +1,45 @@
+﻿
+Console.WriteLine(power(2, 3));
+Console.WriteLine(powerOptimized(50, 100, 13));
+
+
+static int power(int x, int y)
+{
+    int res = 1;     // Initialize result
+
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if ((y & 1) != 0)
+            res = res * x;
+
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = x * x;  // Change x to x^2
+    }
+    return res;
+}
+
+static int powerOptimized(int x, int y, int p)
+{
+    int res = 1; // Initialize result
+
+    x = x % p; // Update x if it is more than or
+               // equal to p
+
+    if (x == 0)
+        return 0; // In case x is divisible by p;
+
+    while (y > 0)
+    {
+
+        // If y is odd, multiply x with result
+        if ((y & 1) != 0)
+            res = (res * x) % p;
+
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
---
+++ PigionholePrinciple/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[] fs = new int[1000000];
+
+void FindNumber(int m)
+{
+    int cur_len = 0;
+    for (int i = 0; i <= m; i++)
+    {
+        cur_len = (cur_len * 10 + 1) % m;
+
+        if (cur_len == 0)
+        {
+            for (int j = 1; j <= i; j++)
+                Console.WriteLine(1);
+            return;
+        }
+        if (fs[cur_len] != 0)
+        {
+            for (int j = 1; j <= i - fs[cur_len]; j++)
+                Console.WriteLine(1);
+            for (int j = 1; j <= fs[cur_len]; j++)
+                Console.WriteLine(1);
+        }
+        fs[cur_len] = i;
+    }
+}
+
+
+FindNumber(5);
